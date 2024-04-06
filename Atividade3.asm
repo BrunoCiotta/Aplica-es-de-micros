51163d41ec4db35f2efdfc3abf266508edf66e6f
Marcus-Reiss <99103633+Marcus-Reiss@users.noreply.github.com>
2024-04-06 18:39:42 -0300

    Update Atividade3.asm

--- Atividade3.asm
+++ Atividade3.asm
@@ -2,16 +2,16 @@ org 0000h
 
 inicio:
 
-MOV A,#01010101b
-MOV B,#10010101b
-ANL A,B
-RR A
-RR A
-CPL A
-RL A
-RL A
-ORL A,B
-XRL A,B
-SWAP A
-JMP inicio
+MOV A,#01010101b ; duração - 1us
+MOV B,#10010101b ; duração - 1us
+ANL A,B ; duração - 1us
+RR A ; duração - 1us
+RR A ; duração - 1us
+CPL A ; duração - 1us
+RL A ; duração - 1us
+RL A ; duração - 1us
+ORL A,B ; duração - 1us
+XRL A,B ; duração - 1us
+SWAP A ; duração - 1us
+JMP inicio ; duração - 1us
 end
