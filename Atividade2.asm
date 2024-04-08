5c7bd92d4d821c218e47afa1e9aa8e15bb8b6922
Marcus-Reiss <99103633+Marcus-Reiss@users.noreply.github.com>
2024-04-08 16:38:59 -0300

    Update Atividade2.asm

--- Atividade2.asm
+++ Atividade2.asm
@@ -1,21 +1,21 @@
 org 00h
 
 inicio:
-MOV A, #2 ; duraÁ„o - 1us
-MOV B, #3 ; duraÁ„o - 2us
-MOV 20h, #7 ; duraÁ„o - 2us
-ADD A, 20h ; duraÁ„o - 1us
-DEC A ; duraÁ„o - 1us
-DEC A ; duraÁ„o - 1us
-DEC A ; duraÁ„o - 1us
-INC B ; duraÁ„o - 1us
-SUBB A, B ; duraÁ„o - 1us
-MUL AB ; duraÁ„o - 4us
-INC B ; duraÁ„o - 1us
-INC B ; duraÁ„o - 1us
-DIV AB ; duraÁ„o - 4us
-MOV 21h, A ; duraÁ„o - 1us
-MOV 22h, B ; duraÁ„o - 2us
-JMP inicio ; duraÁ„o - 2us
+MOV A, #2     ;Move de forma imediata o valor 2 em decimal para o ACC - dura√ß√£o: 1us (1 ciclo)
+MOV B, #3     ;Move de forma imediata o valor 3 em decimal para B - dura√ß√£o: 2us (2 ciclos)
+MOV 20h, #7   ;Move para o endere√ßo 20h o valor 7 em decimal - dura√ß√£o: 2us (2 ciclos)
+ADD A, 20h    ;Soma o conteudo do endereco 20h ao ACC - dura√ß√£o: 1us (1 ciclo)
+DEC A         ;Decrementa o ACC de uma unidade - dura√ß√£o: 1us (1 ciclo)
+DEC A         ;Decrementa o ACC de uma unidade - dura√ß√£o: 1us (1 ciclo)
+DEC A         ;Decrementa o ACC de uma unidade - dura√ß√£o: 1us (1 ciclo)
+INC B         ;Incrementa B em uma unidade - dura√ß√£o: 1us (1 ciclo)
+SUBB A, B     ;Subtrai A por B - dura√ß√£o: 1us (1 ciclo)
+MUL AB        ;Multiplica A por B - dura√ß√£o: 4us (4 ciclos)
+INC B         ;Incrementa B de uma unidade - dura√ß√£o: 1us (1 ciclo)
+INC B         ;Incrementa B de uma unidade - dura√ß√£o: 1us (1 ciclo)
+DIV AB        ;Divide A por B dura√ß√£o: 4us (4 ciclos)
+MOV 21h, A    ;Move o conteudo do ACC para o endereco 21h - dura√ß√£o: 1us (1 ciclo)
+MOV 22h, B    ;Move o conteudo de B para o endereco 22h - dura√ß√£o: 2us (2 ciclos)
+JMP inicio    ;Salta para a label inicio - dura√ß√£o: 2us (2 ciclos)
 
-end
+end           ;Fim do programa - duracao total: 26us
