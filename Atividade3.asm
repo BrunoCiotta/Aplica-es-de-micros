d7041374cca6055c39b1998578620bfd7b417dad
Marcus-Reiss <99103633+Marcus-Reiss@users.noreply.github.com>
2024-04-11 11:42:57 -0300

    Update Atividade3.asm

--- Atividade3.asm
+++ Atividade3.asm
@@ -3,7 +3,7 @@ org 0000h
 inicio:
 
 MOV A,#01010101b    ;Move de forma imediata para o ACC o valor binario indicado - duração: 1us (1 ciclo)
-MOV B,#10010101b    ;Move de forma imediata para B o valor binario indicado - duração: 1us (1 ciclo)
+MOV B,#10010101b    ;Move de forma imediata para B o valor binario indicado - duração: 2us (2 ciclos)
 ANL A,B             ;Realiza o AND logico entre A e B - duração: 1us (1 ciclo)
 RR A                ;Rotaciona A a direita em 1 bit - duração: 1us (1 ciclo)
 RR A                ;Rotaciona A a direita em 1 bit - duração: 1us (1 ciclo)
@@ -13,6 +13,6 @@ RL A                ;Rotaciona A a esquerda em 1 bit - duração: 1us (1 ciclo)
 ORL A,B             ;Realiza o OR logico entre A e B - duração: 1us (1 ciclo)
 XRL A,B             ;Realiza o XOR logico entre A e B - duração: 1us (1 ciclo)
 SWAP A              ;Realiza o SWAP de A - duração: 1us (1 ciclo)
-JMP inicio          ;Salta para a label inicio - duração: 1us (1 ciclo)
+JMP inicio          ;Salta para a label inicio - duração: 2us (2 ciclos)
 
-end                 ;Fim do programa - duracao total: 12us
+end                 ;Fim do programa - duracao total: 14us
